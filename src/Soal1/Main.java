2dec86c8e9b9e14b0c0270f67dfe599abab0354c
DP5-Frederick-038 <godivafrederick@gmail.com>
2024-05-09 19:13:59 +0700

    docs: added documentations to the code in package Soal1

--- src/Soal1/Main.java
+++ src/Soal1/Main.java
@@ -2,17 +2,38 @@ package Soal1;
 
 public class Main {
     public static void main(String[] args) {
+        // Person Object (without parameters)
         Person non = new Person();
-        non.display();
+        non.display();  // called display method from Person class
 
         System.out.println();
 
+        // Person object (with parameters)
         Person Frederick = new Person("Frederick Godiva", 20, "Universitas Sumatera Utara");
-        Frederick.display();
+        Frederick.display();    // called display method from Person class
 
         System.out.println();
 
+        // LaboratoryAssistant object
         LaboratoryAssistant FG = new LaboratoryAssistant("Frederick Godiva", 20, "Universitas Sumatera Utara", "IKLC", 2, 3);
-        FG.display();
+        FG.display();    // called display method from LaboratoryAssistant class
     }
 }
+
+// Narrative Explanation
+/*
+ * Within the main method, we instantiate both the Person and the LaboratoryAssistant classes.
+ *
+ * The Person class encapsulates attributes such as name, age, and university. Moreover, it employs constructor overloading, ensuring that when the class is instantiated, the first method called is the constructor.
+ * If the constructor lacks parameters, the name defaults to "Anonymous", the age to a random number, and the university to Unknown University. Conversely, when parameters are provided, the attributes are initialized accordingly.
+ * Access modifiers govern attribute access, while Setter and Getter methods manage attribute content, promoting encapsulation.
+ * Finally, a display method showcases the information stored within its attributes to the console.
+ *
+ * The LaboratoryAssistant class extends Person, signifying inheritance of methods and attributes from the Person class.
+ * It introduces additional attributes like workPlace, teachingExperience, and numberOfClasses, each with private access modifiers.
+ * A constructor, requiring parameters such as name, age, university, workPlace, teachingExperience, and numberOfClasses, efficiently initializes these attributes.
+ * Utilizing the super() method, the superclass's constructor handles name, age, and university, allowing seamless integration of the provided parameters into the appropriate attributes.
+ * Encapsulation is further emphasized through Setter and Getter methods, facilitating attribute manipulation.
+ * Lastly, a display method, although sharing its name with the Person class's method, serves the purpose of presenting the class's attribute information on the console, illustrating method overriding.
+ *
+ */
--- src/Soal1/Person.java
+++ src/Soal1/Person.java
@@ -1,48 +1,60 @@
 package Soal1;
 
+// Import library for random method
 import java.lang.Math;
 
 public class Person {
+    // Attribute Encapsulation
     private String name;
     private int age;
     private String university;
 
+    // Constructor Overloading
+    // Constructor for class Person without any parameters
     public Person() {
         this.name = "Anonymous";
         this.age = (int) (Math.random() * 100 + 1);
         this.university = "Unknown University";
     }
 
+    // Constructor for class Person with parameters
     public Person(String name, int age, String university) {
         this.name = name;
         this.age = age;
         this.university = university;
     }
 
+    // Setter Method for name attribute
     public void setName(String name) {
         this.name = name;
     }
 
+    // Setter Method for age attribute
     public void setAge(int age) {
         this.age = age;
     }
 
+    // Setter Method for university attribute
     public void setUniversity(String university) {
         this.university = university;
     }
 
+    // Getter Method for name attribute
     public String getName() {
         return this.name;
     }
 
+    // Getter Method for age attribute
     public int getAge() {
         return this.age;
     }
 
+    // Getter Method for university attribute
     public String getUniversity() {
         return this.university;
     }
 
+    // Display Method
     public void display() {
         System.out.println("Hey there, I'm " + this.getName());
         System.out.println("I'm " + this.getAge() + " years old.");
@@ -50,11 +62,14 @@ public class Person {
     }
 }
 
+// Inheritance
 class LaboratoryAssistant extends Person {
+    // Attribute Encapsulation
     private String workPlace;
     private int teachingExperience;
     private int numberOfClasses;
 
+    // Constructor
     public LaboratoryAssistant(String name, int age, String university, String workPlace, int teachingExperience, int numberOfClasses) {
         super(name, age, university);
         this.workPlace = workPlace;
@@ -62,30 +77,37 @@ class LaboratoryAssistant extends Person {
         this.numberOfClasses = numberOfClasses;
     }
 
+    // Setter Method for workPlace
     public void setWorkPlace(String workPlace) {
         this.workPlace = workPlace;
     }
 
+    // Setter Method for teachingExperience
     public void setTeachingExperience(int teachingExperience) {
         this.teachingExperience = teachingExperience;
     }
 
+    // Setter Method for numberOfClasses
     public void setNumberOfClasses(int numberOfClasses) {
         this.numberOfClasses = numberOfClasses;
     }
 
+    // Getter Method for workPlace
     public String getWorkPlace() {
         return this.workPlace;
     }
 
+    // Getter Method for teachingExperience
     public int getTeachingExperience() {
         return this.teachingExperience;
     }
 
+    // Getter Method for numberOfClasses
     public int getNumberOfClasses() {
         return this.numberOfClasses;
     }
 
+    // Method Overriding for display method
     @Override
     public void display() {
         System.out.println("Greetings, I am " + this.getName() + ", currently serving as a laboratory assistant at the " + this.getWorkPlace() + ".");
