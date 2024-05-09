22c691ef7f6ec240c9d72051ff77c5c2a05f2077
DP5-Frederick-038 <godivafrederick@gmail.com>
2024-05-09 19:48:15 +0700

    feat: completed PalindromeChecker Class in package Soal2

--- src/Soal2/PalindromeChecker.java
+++ src/Soal2/PalindromeChecker.java
@@ -1,4 +1,30 @@
 package Soal2;
 
+import java.util.Scanner;
+
 public class PalindromeChecker {
+    public static boolean isPalindrome(String word) {
+        int length = word.length();
+        for (int i = 0; i < length / 2; i++) {
+            if (word.charAt(i) != word.charAt(length - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String choice;
+
+        do {
+            System.out.print("Enter a word : ");
+            String word = input.nextLine();
+
+            System.out.println(isPalindrome(word) ? "This word is a palindrome." : "This word is not a palindrome.");
+
+            System.out.print("Do you want to continue? [Y/N]: ");
+            choice = input.nextLine();
+        } while (choice.equalsIgnoreCase("Y"));
+    }
 }
